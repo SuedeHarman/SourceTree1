8446c865121d6636b6abc0f76bf91e422d30a065
Suede Harman <suedeharman@gmail.com>
2016-01-26 18:26:48 -0700

    Unity Stuff

---
+++ DGM 1600 Intro to Scripting/Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    int myInt = 6;
+
+    void Start()
+    {
+        myInt = Multiply(myInt);
+        Debug.Log(myInt);
+    }
+
+    int Multiply (int number)
+    {
+        int sum;
+        sum = number * 5;
+        return sum;
+    }
+}
---
+++ DGM 1600 Intro to Scripting/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    int myInt = 6;
+
+    void Start()
+    {
+        myInt = Multiply(myInt);
+        Debug.Log(myInt);
+    }
+
+    int Multiply (int number)
+    {
+        int sum;
+        sum = number * 2;
+        return sum;
+    }
+}
