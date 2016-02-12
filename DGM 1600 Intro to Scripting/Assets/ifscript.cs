bcec9ea5134468dc53ca71028a5ba47d3628343f
Suede Harman <suedeharman@gmail.com>
2016-02-11 17:23:53 -0700

    ifscript Commit

---
+++ DGM 1600 Intro to Scripting/Assets/ifscript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifscript : MonoBehaviour
+{
+    public int battery = 100;
+
+    void Update()
+    {
+        if ( battery == 100)
+        {
+            Debug.Log("Full Power");
+        }
+        else if (battery == 0)
+        {
+            Debug.Log("No Power");
+        }
+        else
+        {
+            Debug.Log("Power Level Dropping");
+        }
+    }
+}
