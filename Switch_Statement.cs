cce32ffe6ad29121ad9ccb2019ea73db797fdf44
Suede Harman <suedeharman@gmail.com>
2016-04-26 18:37:33 -0600

    My switch statement script

---
+++ Switch_Statement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch_Statement : MonoBehaviour
+{
+
+    public int thing = 3;
+
+    void Start ()
+    {
+        DoAThing();
+    }
+
+	void DoAThing ()
+    {
+        switch (thing)
+        {
+            case 3:
+                print("Did a thing!");
+                break;
+            case 2:
+                print("Half-did a thing");
+                break;
+            case 1:
+                print("Tried to do animation thing");
+                break;
+            default:
+                print("Did not do a thing");
+                break;
+         }
+	}
+	
+}
